00b7e9768ae750d4713d0e669266078d343e6dd4
Gary Conlon <gconlon+tsn@proofpoint.com>
2025-12-12 16:16:08 +0000

    fix: Handle empty s3_bucket_name variable (#3)
    * fix: Handle empty s3_bucket_name variable to prevent split on null or empty string
    
    * fix: Simplify s3_bucket_names logic using try() to handle null or empty values

--- modules/coralogix-aws-shipper/local.tf
+++ modules/coralogix-aws-shipper/local.tf
@@ -57,5 +57,5 @@ locals {
   is_sqs_integration = var.sqs_name != null && (var.integration_type == "S3" || var.integration_type == "CloudTrail" || var.integration_type == "Sqs") ? true : false
 
   arn_prefix      = "arn:${data.aws_partition.current.partition}"
-  s3_bucket_names = var.s3_bucket_name != null ? toset(split(",", var.s3_bucket_name)) : toset([])
+  s3_bucket_names = try(toset(split(",", var.s3_bucket_name)), toset([]))
 }
